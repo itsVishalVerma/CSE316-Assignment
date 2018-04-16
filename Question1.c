d5f09fe7baf6c6fef1190b2c4f1d05eff9c5dbc1
Vishal Verma <38395432+itsVishalVerma@users.noreply.github.com>
2018-04-16 17:35:58 +0530

    Update Question1.c

--- Question1.c
+++ Question1.c
@@ -2,11 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/types.h>
-#include <unistd.h>
-#include <process.h>
-#include <sys/types.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include<unistd.h>
 
 int main() {
     pid_t pid; // Store the process ID.
